89c7a169d053dcf0bf1587f7a91d3cb8a73f6fc5
TMA\lquangbao <lquangbao@tma.com.vn>
2019-02-27 16:26:00 +0700

    Add Header basic control

--- web/src/components/common/Header.vue
+++ web/src/components/common/Header.vue
@@ -1,7 +1,17 @@
 <template>
-    <el-header>
-
-    </el-header>
+  <el-header>
+    <div class="flex full-width">
+      <div class="home-link flex-item">
+         <a href="/">OFriends</a>
+      </div>
+      <div class="menu-link flex-item">
+        <!-- TODO: replace with a Cog icon -->
+        <a href="/">Config</a>
+        <!-- TODO: replace with a account icon -->
+        <a href="/">Account</a>
+      </div>
+    </div>
+  </el-header>
 </template>
 
 <script>
@@ -12,6 +22,27 @@ export default {
 
 <style lang="scss">
 .el-header {
-  background-color: $--primary-color
+  background-color: $--primary-color;
+  min-height: 36px;
+  @include flex
+}
+.home-link {
+  min-height: 36px;
+  text-align: left;
+  a {
+    color: $--white;
+    font-size: $--font-bg;
+    text-decoration: none;
+  }
+}
+.menu-link {
+  min-height: 36px;
+  text-align: right;
+  a {
+    color: $--white;
+    font-size: $--font-md;
+    text-decoration: none;
+    margin-left: 20px;
+  }
 }
 </style>
