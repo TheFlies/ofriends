be89a6ea0137cfc8ca1113aeec55a7426247bcc1
gaconkzk <gaconkzk@gmail.com>
2019-03-15 10:30:48 +0700

    add login and 404 template

--- web/src/router.js
+++ web/src/router.js
@@ -3,66 +3,80 @@ import Router from 'vue-router'
 import Gifts from './views/Gifts.vue'
 import Hello from '@/components/Hello'
 
+Vue.use(Router)
+
 import Layout from '@/views/layout/Layout'
 
-Vue.use(Router)
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in subMenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if false, the item will hidden in breadcrumb(default is true)
+  }
+**/
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    redirect: 'home',
+    component: Layout,
+    children: [
+      {
+        path: 'home',
+        component: Hello,
+        meta: {
+          title: 'Home',
+          icon: 'form'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/friend',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'Friend',
+        component: () => import('./views/friends/Add.vue'),
+        meta: {
+          title: 'Friends',
+          icon: 'user'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/gifts',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'gifts',
+        component: Gifts,
+        meta: {
+          title: 'Gifts',
+          icon: 'eye'
+        }
+      }
+    ]
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
 
 export default new Router({
   mode: 'history',
+  scrollBehavior: () => ({ y: 0 }),
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      redirect: 'home',
-      component: Layout,
-      children: [
-        {
-          path: 'home',
-          component: Hello,
-          meta: {
-            title: 'Home',
-            icon: 'form'
-          }
-        }
-      ]
-    },
-    {
-      path: '/friend',
-      component: Layout,
-      children: [
-        {
-          path: '',
-          name: 'Friend',
-          component: () => import('./views/friends/Add.vue'),
-          meta: {
-            title: 'Friends',
-            icon: 'user'
-          }
-        }
-      ]
-    },
-    {
-      path: '/gifts',
-      component: Layout,
-      children: [
-        {
-          path: '',
-          name: 'gifts',
-          component: Gifts,
-          meta: {
-            title: 'Gifts',
-            icon: 'eye'
-          }
-        }
-      ]
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes: constantRouterMap
 })
